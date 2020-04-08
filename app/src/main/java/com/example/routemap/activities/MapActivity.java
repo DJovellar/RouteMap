e2c6ff95924783f61b35852282a8e50908b1e5b5
David Jovellar <davidjovellar93@gmail.com>
2020-04-08 15:02:58 +0200

    Implement personalized toolbar with custom buttons in Activity Map

--- app/src/main/java/com/example/routemap/activities/MapActivity.java
+++ app/src/main/java/com/example/routemap/activities/MapActivity.java
@@ -1,15 +1,19 @@
-package com.example.routemap;
+package com.example.routemap.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
+import com.example.routemap.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private Toolbar toolbar;
     private GoogleMap map;
 
     @Override
@@ -17,10 +21,19 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 
     @Override
