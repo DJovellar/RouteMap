f4eb14de0e24bbc4546eb195d9b63f124604d0d3
David Jovellar <davidjovellar93@gmail.com>
2020-04-12 00:54:47 +0200

    Implemented personalized Markers

--- app/src/main/java/com/example/routemap/activities/MapActivity.java
+++ app/src/main/java/com/example/routemap/activities/MapActivity.java
@@ -1,22 +1,42 @@
 package com.example.routemap.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.routemap.R;
+import com.example.routemap.domain.InfoMarker;
+import com.example.routemap.domain.User;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.*;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, OnMapClickListener, OnMarkerClickListener, OnInfoWindowClickListener {
 
     private Toolbar toolbar;
     private GoogleMap map;
 
+    private View v1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +60,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.filterButton:
+                Toast.makeText(this, "Pulsado el boton de filtrar", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
@@ -47,5 +79,103 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         map.setMyLocationEnabled(true);
         map.getUiSettings().setZoomControlsEnabled(true);
         map.getUiSettings().setMyLocationButtonEnabled(true);
+
+        map.setInfoWindowAdapter(new CustomInfoWindowAdapter(getLayoutInflater().from(this)));
+
+        map.setOnMapClickListener(this);
+        map.setOnMarkerClickListener(this);
+        map.setOnInfoWindowClickListener(this);
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        addPersonalizedMarker(latLng);
+    }
+
+    public void addPersonalizedMarker(final LatLng latLng) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+        v1 = inflater.inflate(R.layout.input_info_marker, null);
+
+        builder.setView(v1).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                InfoMarker infoMarker = new InfoMarker();
+                infoMarker.setType(((Spinner) v1.findViewById(R.id.input_info_type)).getSelectedItem().toString());
+                infoMarker.setDescription(((TextView) v1.findViewById(R.id.input_info_description)).getText().toString());
+                infoMarker.setLevel(((Spinner) v1.findViewById(R.id.input_info_level)).getSelectedItem().toString());
+
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM HH:mm");
+                infoMarker.setDate(sdf.format(new Date()));
+
+                //3º Entrega: Obtener de BBDD
+                infoMarker.setAuthor(new User("admin@admin.es", "admin", "admin"));
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+
+                switch (infoMarker.getLevel()) {
+                    case "Leve":
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                        break;
+                    case "Moderado":
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+                        break;
+                    case "Grave":
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                        break;
+                }
+                map.addMarker(markerOptions).setTag(infoMarker);
+            }
+        }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        marker.showInfoWindow();
+        return true;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        marker.hideInfoWindow();
+    }
+
+    //Custom Info Window for the markers
+    public static class CustomInfoWindowAdapter implements InfoWindowAdapter {
+
+        private LayoutInflater inflater;
+
+        CustomInfoWindowAdapter(LayoutInflater inflater) {
+            this.inflater = inflater;
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            return null;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            View v = inflater.inflate(R.layout.info_window_marker, null);
+
+            //Details of the marker
+            InfoMarker infoMarker = (InfoMarker) marker.getTag();
+            ((TextView)v.findViewById(R.id.info_window_type)).setText("Tipo: " + infoMarker.getType());
+            ((TextView)v.findViewById(R.id.info_window_level)).setText("Nivel:  " + infoMarker.getLevel());
+            ((TextView)v.findViewById(R.id.info_window_description)).setText("Descripción: " + infoMarker.getDescription());
+            ((TextView)v.findViewById(R.id.info_window_date)).setText("Fecha: " + infoMarker.getDate().toString());
+            ((TextView)v.findViewById(R.id.info_window_author)).setText("Creado por: " + infoMarker.getAuthor().getUser());
+
+            return v;
+        }
     }
 }
