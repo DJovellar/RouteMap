7cf9dfa618b4be8c51a75011bed408d062b4d794
David Jovellar <davidjovellar93@gmail.com>
2020-04-12 19:18:37 +0200

    Implemented PreferencesActivity and functionality with Shared Preferences

---
+++ app/src/main/java/com/example/routemap/fragments/PreferencesFragment.java
@@ -0,0 +1,25 @@
+package com.example.routemap.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.routemap.R;
+
+public class PreferencesFragment extends PreferenceFragmentCompat {
+
+    public PreferencesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+}
